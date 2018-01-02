c82a742eff20fdf887d3268a77be8220eea8c334
choopka <choopka.mk@gmail.com>
2018-01-02 06:42:36 +0200

    activated apt-mirror and cleanup script

--- run.sh
+++ run.sh
@@ -5,8 +5,9 @@ echo "Running apt-mirror for $dist distribution..."
 #Update dist to download
 sed -i 's/xenial/'$dist'/g' /etc/apt/mirror.list
 cat /etc/apt/mirror.list
+
 #Run apt-mirror
-#/usr/bin/perl /usr/bin/apt-mirror
+/usr/bin/perl /usr/bin/apt-mirror
 
 #Run cleanup
-#/bin/bash /var/spool/apt-mirror/var/clean.sh
+/bin/bash /var/spool/apt-mirror/var/clean.sh
